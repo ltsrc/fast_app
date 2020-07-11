599e0a37e9c41caa4772cb1770f11d27cc42942e
ltsrc <robin.christol@epitech.eu>
2020-07-11 23:53:46 +0200

    wip

---
+++ frontend/src/pages/recoverPassword.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function RecoverPassword() {
+  const classes = useStyles();
+
+  return (
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            id='email'
+            label='Email Address'
+            name='email'
+            autoComplete='email'
+            autoFocus
+          />
+        </form>
+        <Button
+          type='submit'
+          fullWidth
+          variant='contained'
+          color='primary'
+          className={classes.submit}
+        >
+          Recover
+          </Button>
+      </div>
+    </Container>
+  );
+}
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,46 +1,35 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-
-
-function Redirector(path: string) {
-  return () => { return (<Redirect to={path} />); }
-}
+import Signup from './pages/signup';
+import Signin from './pages/signin';
+import RecoverPassword from './pages/recoverPassword';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' render={Redirector('/home')} />
-        <Route path='/home' render={Home} />
-        <Route path='/login' render={Login} />
-        <Route path='/signin' render={Signin} />
+
+        <Route exact path='/'>
+          <Redirect to='/home' />
+        </Route>
+
+        <Route path='/home' />
+
+        <Route path='/signin' >
+          <Signin />
+        </Route>
+
+        <Route path='/signup' >
+          <Signup />
+        </Route>
+
+        <Route path='/recover-password' >
+          <RecoverPassword />
+        </Route>
+
         <Route render={NotFound} />
       </Switch>
     </BrowserRouter >
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      Home
-    </div>
-  );
-}
-
-function Login() {
-  return (
-    <div>
-      Login
-    </div>
-  );
-}
-
-function Signin() {
-  return (
-    <div>
-      Signin
-    </div>
   );
 }
 
